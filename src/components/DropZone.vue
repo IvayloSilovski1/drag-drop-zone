dfc4c12bea88116a1aece6d7bf6bd82c509a0fb3
IvayloSilovski1 <ivaylo.silovski689@gmail.com>
2021-08-30 14:48:58 +0300

    simple drag&drop zone

---
+++ src/components/DropZone.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    @dragenter.prevent="toggleActive"
+    @dragleave.prevent="toggleActive"
+    @drop.prevent="toggleActive"
+    @dragover.prevent
+    class="dropzone"
+    :class="{ 'active-dropzone': active }"
+  >
+    <span>Drag or Drop File</span>
+    <span> OR </span>
+    <label for="dropzoneFile">Select File</label>
+    <input type="file" id="dropzoneFile" class="dropzoneFile" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "DropZone",
+  setup() {
+    const active = ref(false);
+    const toggleActive = () => {
+      active.value = !active.value;
+    };
+    return { active, toggleActive };
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+.dropzone {
+  width: 40rem;
+  height: 20rem;
+  background-color: #ccc;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 1rem;
+  border: 0.3rem dashed #41b883;
+  border-radius: 1.5rem;
+  transition: 0.3s ease all;
+
+  label {
+    padding: 0.5rem 0.7rem;
+    color: #fff;
+    background-color: #41b883;
+    transition: 0.3s ease all;
+  }
+
+  input {
+    display: none;
+  }
+}
+
+.active-dropzone {
+  color: #fff;
+  border-color: #fff;
+  background-color: #41b883;
+
+  label {
+    background-color: #fff;
+    color: #41b883;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Drag and Drop you file into our DropZone</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile" />
+    <span class="file-info">Uploaded File: {{ dropzoneFile.name }}</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DropZone from "../components/DropZone.vue";
+import { ref } from "vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    DropZone,
+  },
+
+  setup() {
+    let dropzoneFile = ref("");
+
+    const drop = (e) => {
+      dropzoneFile.value = e.dataTransfer.files[0];
+    };
+
+    const selectedFile = () => {
+      dropzoneFile.value = document.querySelector(".dropzoneFile").files[0];
+    };
+
+    return { dropzoneFile, drop, selectedFile };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+
+  h1 {
+    margin: 2rem auto;
+    font-size: 2rem;
+    width: 40rem;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+  }
+
+  .file-info {
+    margin-top: 2rem;
   }
 }
-</script>
+</style>
